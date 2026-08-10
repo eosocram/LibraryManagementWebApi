666a58ae7059ebf845ac580da821ed4ef771aac9
eosocram <socramlmonteiro@gmail.com>
2026-08-10 16:34:47 -0300

    create BooksController

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace LibraryManagement.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class BooksController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok();
+    }
+}
