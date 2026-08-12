122755a62bd4c3715fb4c296fd977a830ea15f40
eosocram <socramlmonteiro@gmail.com>
2026-08-12 14:19:06 -0300

    implement Get book by id

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -6,27 +6,44 @@ namespace LibraryManagement.Api.Controllers;
 [Route("api/[controller]")]
 public class BooksController : ControllerBase
 {
+
+
+    private List<Book> books = new()
+    {
+        new Book
+        {
+        Title = "Title1",
+        Author = "Author1",
+        Id = 1
+        },
+
+        new Book
+        {
+            Title = "Title2",
+            Author = "Author2",
+            Id = 2
+        }
+    };
     [HttpGet]
     public IActionResult Get()
     {
-        var book1 = new Book
-        {
-            Title = "Title1",
-            Author = "Author1"
-        };
-
-        var book2 = new Book
-        {
-            Title = "Title2", 
-            Author = "Author2"
-        };
-
-        var books = new List<Book>
-        {
-            book1,
-            book2
-        };
-        
         return Ok(books);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetById(int id)
+    {
+        Book? book = books.FirstOrDefault(book => book.Id == id);
+        if (book == null)
+        {
+            return NotFound();
+        }
+        return Ok(book);
+    }
+
+    [HttpPost]
+    public IActionResult Post(Book book)
+    {
+        return Created("api/books/15",book);
     }
 }
--- Models/Book.cs
+++ Models/Book.cs
@@ -4,10 +4,5 @@ public class Book
 {
     public string Title { get; set; } = string.Empty;
     public string Author { get; set; } = string.Empty;
-
-    // public Book(string title, string author)
-    // {
-    //     Title = title;
-    //     Author = author;
-    // }
+    public int Id { get; set; }
 }
